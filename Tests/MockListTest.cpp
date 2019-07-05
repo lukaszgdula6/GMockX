fffaecb9472c2ff426c186e652ba7b51339d95b3
Lukasz <lukasz@gdula.org>
2019-07-05 17:14:34 +0200

    MockListTest became MockListTestSuite

--- Tests/MockListTest.cpp
+++ Tests/MockListTest.cpp
@@ -10,32 +10,45 @@ struct Mock1 {};
 struct Class2 {};
 struct Mock2 {};
 
-typedef MockList<Mock1, Class1> MockList1;
-typedef MockList1::NoMockException NoMockException1;
-typedef MockList1::PendingMockException PendingMockException1;
-
-TEST(MockListTest, shouldThrowWhenNoMocksRegistered)
+TEST(MockListTest, shouldHaveInstance)
 {
-    Class1 c;
-    EXPECT_THROW(MockList1::instance().findMockFor(&c), NoMockException1);
+    EXPECT_THAT((&MockList<Mock1, Class1>::instance()), Not(IsNull()));
+    EXPECT_THAT((&MockList<Mock2, Class2>::instance()), Not(IsNull()));
+    EXPECT_NE(reinterpret_cast<void*>(&MockList<Mock1, Class1>::instance()),
+              reinterpret_cast<void*>(&MockList<Mock2, Class2>::instance()));
 }
 
-TEST(MockListTest, shouldAllowUnregisterNotRegisteredMock)
+struct MockListTestSuite : Test
+{
+    typedef MockList<Mock1, Class1> MockList1;
+    typedef MockList1::NoMockException NoMockException1;
+    typedef MockList1::PendingMockException PendingMockException1;
+
+    MockList1 mockList1;
+};
+
+TEST_F(MockListTestSuite, shouldThrowWhenNoMocksRegistered)
+{
+    Class1 c;
+    EXPECT_THROW(mockList1.findMockFor(&c), NoMockException1);
+}
+
+TEST_F(MockListTestSuite, shouldAllowUnregisterNotRegisteredMock)
 {
     Mock1 m;
-    MockList1::instance().unregisterMock(&m);
+    mockList1.unregisterMock(&m);
 }
 
-TEST(MockListTest, shouldThrowWhenMockNotCalled)
+TEST_F(MockListTestSuite, shouldThrowWhenMockNotCalled)
 {
     Class1 c;
-    EXPECT_THROW(MockList1::instance().findMockFor(&c), NoMockException1);
+    EXPECT_THROW(mockList1.findMockFor(&c), NoMockException1);
 }
 
-TEST(MockListTest, shouldHaveRegisteredMock)
+TEST_F(MockListTestSuite, shouldHaveRegisteredMock)
 {
     Class1 c;
     Mock1 m;
-    MockList1::instance().registerMock(&m);
-    EXPECT_EQ(&m, MockList1::instance().findMockFor(&c));
+    mockList1.registerMock(&m);
+    EXPECT_EQ(&m, mockList1.findMockFor(&c));
 }
